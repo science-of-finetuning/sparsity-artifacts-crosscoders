7a7673cf2f18a142e058ad195211bcaa475acf75
Julian Minder <jminder@ethz.ch>
2024-10-27 17:40:47 +0000

    added and moved training scripts to /scripts

---
+++ scripts/generate_validation_datasets.py
@@ -0,0 +1,43 @@
+import argparse
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from dictionary_learning.cache import ActivationCache
+from datasets import load_from_disk, load_dataset
+from loguru import logger
+import torch as th
+from nnsight import LanguageModel
+from pathlib import Path
+import os
+
+NUM_ROWS = 10**5
+tokenizer = AutoTokenizer.from_pretrained("/dlabscratch1/public/llm_weights/gemma_hf/gemma-2-2b-it")
+
+DATA_PATH = Path('/dlabscratch1/public/datasets/')
+chat_data_path = DATA_PATH / "lmsys-chat-1m-formatted"
+base_data_path = Path("/dlabscratch1/cdumas/.cache/huggingface/datasets/HuggingFaceFW___fineweb/")
+
+# LMSYS Chat
+chat_dataset = load_from_disk(chat_data_path)
+
+# FineWeb
+base_dataset = load_dataset("HuggingFaceFW/fineweb",  name="sample-10BT", split="train", cache_dir=Path("/dlabscratch1/cdumas/.cache/huggingface/datasets/")).select(range(10**6))
+
+# find index for 100m tokens
+idx = 0
+num_toks = 0
+while num_toks < 10**8:
+    num_toks += len(tokenizer(base_dataset[idx]["text"], max_length=1024, truncation=True)["input_ids"])    
+    idx += 1
+    print(num_toks)
+base_idx = idx
+base_dataset.select(range(base_idx+1, base_idx + 1 + NUM_ROWS)).save_to_disk("datasets/validation/fineweb_100m_sample")
+
+
+# find index for 100m tokens
+idx = 0
+num_toks = 0
+while num_toks < 10**8:
+    num_toks += len(tokenizer(chat_dataset[idx]["text"], max_length=1024, truncation=True)["input_ids"])    
+    idx += 1
+    print(num_toks)
+chat_idx = idx
+chat_dataset.select(range(chat_idx+1, chat_idx + 1 + NUM_ROWS)).save_to_disk("datasets/validation/lmsys_chat")
