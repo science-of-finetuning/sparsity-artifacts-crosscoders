f3169f17e389412f966bfe24d5d9ed0751d388de
Clement Dumas <clement.dumas@ens-paris-saclay.fr>
2024-10-31 14:41:38 +0100

    moved to setup to eval file

--- evaluate.py
+++ evaluate.py
@@ -1,143 +1,19 @@
 from dictionary_learning.dictionary import CrossCoder
 from collections import defaultdict
-import einops
 import torch as th
 from tools.split_gemma import split_gemma
 from argparse import ArgumentParser
 from tqdm.auto import tqdm
 import pandas as pd
 import json
-from abc import ABC, abstractmethod
-from torch.nn.functional import relu
 import wandb
 from dlabutils import model_path
 from pathlib import Path
 from torch.nn.functional import kl_div
 from torchmetrics.aggregation import MeanMetric
+from setup_to_eval import *
 
 CHAT_TEMPLATE = open("templates/gemma_chat_template.jinja").read()
-
-
-class HalfStepPreprocessFn(ABC):
-    @abstractmethod
-    def preprocess(
-        self, base_activations, instruct_activations
-    ) -> (
-        tuple[th.Tensor, None]
-        | tuple[None, th.Tensor]
-        | tuple[None, th.Tensor, th.Tensor | None]
-        | tuple[th.Tensor, None, th.Tensor | None]
-    ):
-        """
-        Preprocess the activations before the last half forward.
-        Returns activations, None if the base model should finish the forward pass.
-        Returns None, activations if the instruct model should finish the forward pass.
-        Can also return a third element which is the batch mask of activations that were selected.
-        """
-        raise NotImplementedError
-
-    def __call__(self, base_activations, instruct_activations):
-        res = self.preprocess(base_activations, instruct_activations)
-        if len(res) == 2:  # no mask
-            return res[0], res[1], None
-        elif len(res) == 3:  # mask
-            return res
-        else:
-            raise ValueError(f"Expected 2 or 3 elements, got {len(res)}")
-
-
-class IdentityPreprocessFn(HalfStepPreprocessFn):
-    def __init__(self, continue_with_base: bool = False):
-        self.continue_with_base = continue_with_base
-
-    def continue_with_model(self, result):
-        return (result, None) if self.continue_with_base else (None, result)
-
-    def preprocess(self, base_activations, instruct_activations):
-        return (
-            (base_activations, None)
-            if self.continue_with_base
-            else (None, instruct_activations)
-        )
-
-
-class SwitchPreprocessFn(HalfStepPreprocessFn):
-    def __init__(self, continue_with_base: bool = False):
-        self.continue_with_base = continue_with_base
-
-    def preprocess(self, base_activations, instruct_activations):
-        return (
-            (instruct_activations, None)
-            if self.continue_with_base
-            else (None, base_activations)
-        )
-
-
-class CrossCoderReconstruction(IdentityPreprocessFn):
-    def __init__(
-        self,
-        crosscoder: CrossCoder,
-        reconstruct_with_base: bool = False,
-        continue_with_base: bool = False,
-    ):
-        super().__init__(continue_with_base)
-        self.crosscoder = crosscoder
-        self.reconstruct_with_base = reconstruct_with_base
-
-    def preprocess(self, base_activations, instruct_activations):
-        cc_input = th.stack(
-            [base_activations, instruct_activations], dim=2
-        ).float()  # b, seq, 2, d
-        cc_input = einops.rearrange(cc_input, "b s m d -> (b s) m d")
-        f = self.crosscoder.encode(cc_input)  # (b s) D
-        reconstruction = th.einsum(
-            "bD, Dd->bd",
-            f,
-            self.crosscoder.decoder.weight[0 if self.reconstruct_with_base else 1],
-        )  # (b s) d
-        reconstruction = einops.rearrange(
-            reconstruction, "(b s) d -> b s d", b=base_activations.shape[0]
-        )
-        return self.continue_with_model(reconstruction.bfloat16())
-
-
-class CrossCoderSteeringFeature(IdentityPreprocessFn):
-    def __init__(
-        self,
-        crosscoder: CrossCoder,
-        steer_base_activations: bool,
-        steer_with_base_features: bool,
-        features_to_steer: list[int] | None,
-        continue_with_base: bool,
-        filter_treshold: float | None = None,
-    ):
-        super().__init__(continue_with_base)
-        if features_to_steer is None:
-            features_to_steer = list(range(crosscoder.decoder.weight.shape[1]))
-        self.encoder_weight = crosscoder.encoder.weight[:, :, features_to_steer]  # ldf
-        self.decoder_weight = crosscoder.decoder.weight[:, features_to_steer]  # lfd
-        self.steer_with_base_features = steer_with_base_features
-        self.steer_base_activations = steer_base_activations
-        self.filter_treshold = filter_treshold
-    def preprocess(self, base_activations, instruct_activations):
-        cc_input = th.stack(
-            [base_activations, instruct_activations], dim=2
-        ).float()  # b, seq, 2, d
-        f = relu(
-            th.einsum("bsld, ldf -> bsf", cc_input, self.encoder_weight)
-        )  # b, seq, f
-        if self.filter_treshold is not None:
-            mask = f.sum(dim=-1).max(dim=-1).values > self.filter_treshold
-            if not mask.any():
-                return None, None, None
-            f = f[mask]
-        decoded = th.einsum("bsf, lfd -> bsld", f, self.decoder_weight)
-        steering_feature = decoded[:, :, 1, :] - decoded[:, :, 0, :]
-        if self.steer_with_base_features:
-            steering_feature = -steering_feature
-        act = base_activations if self.steer_base_activations else instruct_activations
-        res = act[mask] + steering_feature
-        return self.continue_with_model(res.bfloat16())
 
 
 @th.inference_mode()
@@ -262,9 +138,11 @@ def evaluate_interventions(
             fn_name,
             preprocess_before_last_half_fn,
         ) in preprocess_before_last_half_fns.items():
-            base_activations_edited, instruct_activations_edited = (
+            base_activations_edited, instruct_activations_edited, mask = (
                 preprocess_before_last_half_fn(base_activations, instruct_activations)
             )
+            if mask is not None:
+                
             final_out = None
             if base_activations_edited is not None:
                 final_out = base_model.second_half_forward(
@@ -333,6 +211,8 @@ def evaluate_interventions(
                 perplexity_wrt_instruct_pred[fn_name].update(
                     th.exp(loss_wrt_instruct_pred)
                 )
+                num_samples[fn_name] += n_pred
+                wandb.log({f"num_samples/{fn_name}": num_samples[fn_name]}, step=i)
             if i % log_every == 0:
                 wandb.log(
                     {
@@ -389,191 +269,6 @@ def evaluate_interventions(
     return compute_result()
 
 
-def create_half_fn_dict_main(
-    crosscoder: CrossCoder,
-    it_only_features: list[int],
-    base_only_features: list[int],
-) -> dict[str, HalfStepPreprocessFn]:
-    half_fns = {}
-
-    half_fns["1-instruct"] = IdentityPreprocessFn(continue_with_base=False)
-
-    # steer the base activations with all features from IT decoder and continue with instruct   ===   replace it error with base error
-    half_fns["2-steer_all"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=False,
-        features_to_steer=None,
-    )
-
-    half_fns["3-base_to_instruct"] = SwitchPreprocessFn(continue_with_base=False)
-
-    # steer the base activations with the IT only features from instruct decoder and continue with instruct
-    half_fns["4-steer_it_only"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=False,
-        features_to_steer=it_only_features,
-    )
-    # 5/ RANDOM
-
-    # instruct reconstruction
-    half_fns["6-instruct_reconstruct"] = CrossCoderReconstruction(
-        crosscoder, reconstruct_with_base=False, continue_with_base=False
-    )
-    # Take the instruct activations and remove the IT only features
-    half_fns["7-remove_it_only"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=False,
-        steer_with_base_features=True,
-        continue_with_base=False,
-        features_to_steer=it_only_features,
-    )
-
-    # steer the base activations with the IT only & base only features from instruct decoder and continue with instruct
-    half_fns["?-steer_it_and_base_only"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=False,
-        features_to_steer=it_only_features + base_only_features,
-    )
-
-    # reconstruct instruct and finish with instruct
-
-    return half_fns
-
-
-def create_half_fn_dict_secondary(
-    crosscoder: CrossCoder, it_only_features: list[int], base_only_features: list[int]
-) -> dict[str, HalfStepPreprocessFn]:
-    half_fns = {}
-    half_fns["base_reconstruct"] = CrossCoderReconstruction(
-        crosscoder, reconstruct_with_base=True, continue_with_base=True
-    )
-    # steer the base activations with the IT only features from instruct decoder and continue with base
-    half_fns["steer_it_only_to_base"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=True,
-        features_to_steer=it_only_features,
-    )
-    # steer the base activations with the IT only & base only features from instruct decoder and continue with base
-    half_fns["steer_it_and_base_only_to_base"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=True,
-        features_to_steer=it_only_features + base_only_features,
-    )
-    # steer the base activations with the base only features from instruct decoder and continue with base
-    half_fns["steer_base_only_to_base"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=True,
-        features_to_steer=base_only_features,
-    )
-    # steer the base activations with all features from IT decoder and continue with base   ===   replace it error with base error
-    half_fns["steer_all_to_base"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=True,
-        features_to_steer=None,
-    )
-    # steer the base activations with the base only features from instruct decoder and continue with instruct
-    half_fns["steer_base_only"] = CrossCoderSteeringFeature(
-        crosscoder,
-        steer_base_activations=True,
-        steer_with_base_features=False,
-        continue_with_base=False,
-        features_to_steer=base_only_features,
-    )
-
-    return half_fns
-
-
-def create_half_fn_dict_seeds(
-    crosscoder: CrossCoder, seeds: list[int], num_features: int
-) -> dict[str, HalfStepPreprocessFn]:
-    half_fns = {}
-    for seed in seeds:
-        th.manual_seed(seed)
-        features_to_steer = th.randperm(crosscoder.decoder.weight.shape[1])[
-            :num_features
-        ]
-        # half_fns[f"5-random_s{seed}"] = CrossCoderSteeringFeature(
-        #     crosscoder,
-        #     steer_base_activations=True,
-        #     steer_with_base_features=False,
-        #     continue_with_base=False,
-        #     features_to_steer=features_to_steer,
-        # )
-        half_fns[f"?-remove_random_s{seed}"] = CrossCoderSteeringFeature(
-            crosscoder,
-            steer_base_activations=False,
-            steer_with_base_features=True,
-            continue_with_base=False,
-            features_to_steer=features_to_steer,
-        )
-    return half_fns
-
-
-def create_half_fn_dict_no_cross() -> dict[str, HalfStepPreprocessFn]:
-    half_fns = {}
-    half_fns["base"] = IdentityPreprocessFn(continue_with_base=True)
-    half_fns["instruct - debugging"] = IdentityPreprocessFn(continue_with_base=False)
-    half_fns["instruct_to_base"] = SwitchPreprocessFn(continue_with_base=True)
-    return half_fns
-
-
-def create_it_only_ft_fn_dict(
-    crosscoder: CrossCoder, it_only_features: list[int], base_only_features: list[int]
-) -> dict[str, HalfStepPreprocessFn]:
-    half_fns = {
-        "remove_it_only_custom": CrossCoderSteeringFeature(
-            crosscoder,
-            steer_base_activations=False,
-            steer_with_base_features=True,
-            continue_with_base=False,
-            features_to_steer=it_only_features,
-        ),
-        "remove_it_and_base_only_custom": CrossCoderSteeringFeature(
-            crosscoder,
-            steer_base_activations=False,
-            steer_with_base_features=True,
-            continue_with_base=False,
-            features_to_steer=it_only_features + base_only_features,
-        ),
-        "steer_it_only_custom_to_base": CrossCoderSteeringFeature(
-            crosscoder,
-            steer_base_activations=True,
-            steer_with_base_features=False,
-            continue_with_base=True,
-            features_to_steer=it_only_features,
-        ),
-        "steer_it_only_custom": CrossCoderSteeringFeature(
-            crosscoder,
-            steer_base_activations=True,
-            steer_with_base_features=False,
-            continue_with_base=False,
-            features_to_steer=it_only_features,
-        ),
-        "steer_it_and_base_only_custom": CrossCoderSteeringFeature(
-            crosscoder,
-            steer_base_activations=True,
-            steer_with_base_features=False,
-            continue_with_base=False,
-            features_to_steer=it_only_features + base_only_features,
-        ),
-    }
-    return half_fns
-
-
 from datasets import load_from_disk
 
 # from nnsight import LanguageModel
@@ -626,7 +321,7 @@ if __name__ == "__main__":
     )
     dataset = load_from_disk(args.dataset_path)
     if args.test:
-        dataset = dataset.select(range(100))
+        dataset = dataset.select(range(300))
     device = (
         args.device
         if args.device != "auto"
@@ -650,9 +345,22 @@ if __name__ == "__main__":
     # fn_dict.update(
     #     create_half_fn_dict_secondary(crosscoder, it_only_features, base_only_features)
     # )
+    # fn_dict.update(
+    #     create_it_only_ft_fn_dict(crosscoder, it_only_features, base_only_features)
+    # )
+    fn_dict.update(create_half_fn_thresholded_features(crosscoder))
     fn_dict.update(
-        create_it_only_ft_fn_dict(crosscoder, it_only_features, base_only_features)
+        create_half_fn_dict_steer_seeds(
+            crosscoder, seeds, len(INTERESTING_FEATURES), threshold=10
+        )
     )
+    fn_dict.update(
+        create_half_fn_dict_remove_seeds(
+            crosscoder, seeds, len(INTERESTING_FEATURES), threshold=10
+        )
+    )
+    fn_dict.update(create_half_fn_dict_steer_seeds(crosscoder, seeds, 1, threshold=10))
+    fn_dict.update(create_half_fn_dict_remove_seeds(crosscoder, seeds, 1, threshold=10))
     result = evaluate_interventions(
         base_model,
         instruct_model,
