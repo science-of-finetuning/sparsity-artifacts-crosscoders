a5d19e2a36794ecb303a80f561caf2a2046e5420
Julian Minder <jminder@ethz.ch>
2025-01-15 22:56:47 +0100

    added code for stage-2 relproj verification

--- scripts/collect_activations.py
+++ scripts/collect_activations.py
@@ -8,11 +8,13 @@ from nnsight import LanguageModel
 from dlabutils import model_path
 from pathlib import Path
 import os
+
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=True)
     parser.add_argument("--activation-store-dir", type=str, required=True)
+    parser.add_argument("--batch-size", type=int, default=64)
     args = parser.parse_args()
 
 
@@ -20,7 +22,7 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(model_path(args.model))
     model = LanguageModel(model, tokenizer=tokenizer)
     num_layers = int(len(model.model.layers))
-    layers = [int(num_layers*i) for i in (0.2, 0.5, 0.8)]
+    layers = [int(num_layers*i) for i in (0.5,)]
     logger.info(f"Collecting activations from layers: {layers}")
 
     submodules = [model.model.layers[layer] for layer in layers]
@@ -39,9 +41,9 @@ if __name__ == "__main__":
     # LMSYS Chat
     chat_dataset = load_from_disk(chat_data_path)
     out_dir = store_dir / "lmsys_chat"
-    ActivationCache.collect(chat_dataset["text"], submodules, submodule_names, model, out_dir, shuffle_shards=False, io="out", shard_size=10**6, batch_size=16, context_len=1024, d_model=d_model, last_submodule=submodules[-1])
+    ActivationCache.collect(chat_dataset["text"], submodules, submodule_names, model, out_dir, shuffle_shards=False, io="out", shard_size=10**6, batch_size=args.batch_size, context_len=1024, d_model=d_model, last_submodule=submodules[-1])
 
     # # FineWeb
     base_dataset = load_dataset("HuggingFaceFW/fineweb",  name="sample-10BT", split="train", cache_dir=Path("/dlabscratch1/cdumas/.cache/huggingface/datasets/")).select(range(10**6))
     out_dir = store_dir / "fineweb"
-    ActivationCache.collect(base_dataset["text"], submodules, submodule_names, model, out_dir, shuffle_shards=False, io="out", shard_size=10**6, batch_size=16, context_len=1024, d_model=d_model, last_submodule=submodules[-1])
+    ActivationCache.collect(base_dataset["text"], submodules, submodule_names, model, out_dir, shuffle_shards=False, io="out", shard_size=10**6, batch_size=args.batch_size, context_len=1024, d_model=d_model, last_submodule=submodules[-1])
