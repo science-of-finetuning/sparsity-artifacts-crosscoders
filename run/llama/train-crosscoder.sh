ddab8104b7e39f269ea4bbfff789d4b7c948557d
Julian Minder <julian.minder@icloud.com>
2025-05-07 12:56:30 +0000

    update to training scripts

--- run/llama/train-crosscoder.sh
+++ run/llama/train-crosscoder.sh
@@ -7,19 +7,23 @@ BATCH_SIZE=2048
 WORKERS=32
 # Model configuration
 ACTIVATION_DIR="$DATASTORE/activations/"
-LAYER=13
-BASE_MODEL="google/gemma-2-2b"
-INSTRUCT_MODEL="google/gemma-2-2b-it"
+LAYER=16
+CHAT_MODEL=meta-llama/Meta-Llama-3.1-8B-Instruct
+BASE_MODEL=meta-llama/Meta-Llama-3.1-8B
 DEVICE="cuda"
 LR=1e-4
 MU=0.041
+TEXT_COLUMN="text_llama3"
 
 # Parse command line arguments to check for custom mu value
 custom_mu=false
+custom_lr=false
 for arg in "$@"; do
     if [[ $arg == --mu* ]]; then
         custom_mu=true
-        break
+    fi
+    if [[ $arg == --lr* ]]; then
+        custom_lr=true
     fi
 done
 
@@ -30,19 +34,21 @@ FLAGS="--activation-store-dir $ACTIVATION_DIR \
 --base-model $BASE_MODEL \
 --chat-model $CHAT_MODEL \
 --same-init-for-all-layers \
---lr $LR \
 --init-with-transpose \
 --validate-every-n-steps 20_000 \
 --epochs 2 \
 --local-shuffling \
 --seed 42 \
---num-samples 100_000_000"
+--num-samples 100_000_000 \
+--text-column $TEXT_COLUMN"
 
 # Only add default mu if not provided in command line arguments
 if [ "$custom_mu" = false ]; then
     FLAGS="$FLAGS --mu $MU"
 fi
-
+if [ "$custom_lr" = false ]; then
+    FLAGS="$FLAGS --lr $LR"
+fi
 additional_flags=$@
 
 python scripts/train_crosscoder.py $FLAGS $additional_flags
