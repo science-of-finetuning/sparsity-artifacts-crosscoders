d8eaefd0473f4e3a3598ab99684ffcf9569ca14a
Julian Minder <julian.minder@icloud.com>
2025-05-21 11:16:32 +0000

    added results and data paths

--- run/llama_8b/pipeline.sh
+++ run/llama_8b/pipeline.sh
@@ -10,4 +10,4 @@ CHAT_MODEL=meta-llama/Meta-Llama-3.1-8B-Instruct
 BASE_MODEL=meta-llama/Meta-Llama-3.1-8B
 TEXT_COLUMN="text_llama3"
 
-python crosscoder_analysis_pipeline.py $@ --base-model $BASE_MODEL --chat-model $CHAT_MODEL --layer $LAYER --lmsys-col $TEXT_COLUMN
+python crosscoder_analysis_pipeline.py $@ --base-model $BASE_MODEL --chat-model $CHAT_MODEL --layer $LAYER --lmsys-col $TEXT_COLUMN --data-dir /workspace/data/ --results-dir /workspace/data/results
